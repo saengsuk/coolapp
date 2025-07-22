1e37b7219671923afbcee8003d0ea79123f7ecce
Thanakorn <thanakorn.s@gmail.com>
2025-07-22 19:03:35 +0700

    Get amplify config

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import {Amplify} from "aws-amplify";
+import output from "./amplify_outputs.json"
+
+
+Amplify.configure(output)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
